7ff5484a2f6809d7b98ce79f9dd750a3e32ff7ea
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2023-02-24 14:26:27 +0100

    Cleanup shader

--- app/src/main/res/raw/shaderfun.glsl
+++ app/src/main/res/raw/shaderfun.glsl
@@ -8,19 +8,19 @@ uniform sampler2D noise;
 uniform float time;
 
 void main() {
+    // UVs for framebuffer and noise texture
     vec2 fbUV = gl_FragCoord.xy / framebuffer_resolution;
-    vec2 noiseUV = gl_FragCoord.xy / 1024.0 / 5.0 + time / 4.0;
+    vec2 noiseUV = gl_FragCoord.xy / 1024.0 / 10.0 + time / 6.0 ;
 
-
+    // Sample noise texture
     vec4 noiseColor = texture2D(noise, noiseUV);
 
+    // Distort UVs using the noise, and resample using distorted UVs
     vec2 uv1 = (noiseColor.xy -0.5);
     vec4 noiseColor2 = texture2D(noise, uv1);
 
-    //uv = vec2(uv.x + sin(uv.y * 20.0) / 20.0, uv.y );
-    vec4 inputColor = texture2D(framebuffer, fbUV + (noiseColor2.xy -0.5)*0.2);
+    // Sample the framebuffer with the doubly distorted UVs
+    vec4 inputColor = texture2D(framebuffer, fbUV + (noiseColor2.xy -0.5)*0.05);
 
-    //gl_FragColor = vec4(noiseColor2.xy,0.0,1.0);
     gl_FragColor = inputColor;
-    //gl_FragColor = texture2D(noise, uv);
 }
